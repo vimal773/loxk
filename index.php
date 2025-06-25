b57158443d75b2ffc044461c62db9bbf897ee4fa
vrundsurani <168046828+vrundsurani@users.noreply.github.com>
2025-06-25 17:25:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$valid_passwords = ["pass123", "admin789", "guest456"];
+$valid_passwords = file("passwords.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $error = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
